b73d794febc7ef024f1903cd560919afbe833e40
Igor Goncharenko <gonya90.gg@gmail.com>
2026-01-13 16:28:51 +0200

    chore: save cleanup script and lint fixes

--- Scripts/Manual/upload_rescue_video.py
+++ Scripts/Manual/upload_rescue_video.py
@@ -1,5 +1,4 @@
 import sys
-import os
 from pathlib import Path
 
 # Setup paths
@@ -9,8 +8,8 @@ SRC_DIR = FACTORY_DIR / "src"
 sys.path.append(str(SRC_DIR / "uploaders"))
 sys.path.append(str(SRC_DIR / "pipeline"))
 
-from youtube_uploader import upload_video
 from account_manager import AccountManager
+from youtube_uploader import upload_video
 
 
 def main():
